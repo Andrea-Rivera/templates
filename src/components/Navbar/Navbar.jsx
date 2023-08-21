7317731af092256f2c030367cdd89ed7ae18380e
Andrea <andita54@hotmail.com>
2023-08-21 21:20:57 +1000

    uploading banner

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false)
   return (
 <nav className="navbar">
-<h3 className="logo">Logo</h3>
+<h3 className="logo">Andrea Rivera</h3>
 <ul className={isMobile ? "nav-links-mobile":"nav-links"}
 onClick = {()=>  setIsMobile(false)}>
     <Link to="/" className="home">
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -4,8 +4,8 @@ import Banner from "../Banner/Banner";
 function Home() {
   return (
     <div>
-      <h1>home</h1>
       <Banner/>
+ 
     </div>
 
   )
